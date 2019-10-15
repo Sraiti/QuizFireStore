9ac2bb809fa121cc21c8b8a4c74bcc62ab37476f
Abdellatif Sraiti <abdellsraiti019@gmail.com>
2019-10-15 13:45:00 +0100

    Added Background Music Successfully
    ALL Credits TO JWIGOU

---
+++ app/src/main/java/com/example/firestorequiz/MusicBackground/MediaPlayerPresenter.java
@@ -0,0 +1,41 @@
+package com.example.firestorequiz.MusicBackground;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+import com.example.firestorequiz.R;
+
+public class MediaPlayerPresenter {
+
+    private static MediaPlayerPresenter mPresenter;
+
+    private MediaPlayer mPlayer;
+
+    private MediaPlayerPresenter(Context context) {
+        mPlayer = MediaPlayer.create(context, R.raw.jingle);
+    }
+
+
+    public static MediaPlayerPresenter getInstance(Context context) {
+        if (mPresenter == null) {
+            mPresenter = new MediaPlayerPresenter(context);
+        }
+        return mPresenter;
+    }
+
+    public void pauseMusic() {
+        if (mPlayer.isPlaying()) {
+            mPlayer.pause();
+        }
+    }
+
+    public boolean isPlaying() {
+
+        return mPlayer.isPlaying();
+    }
+
+    public void playMusic() {
+        mPlayer.start();
+
+    }
+}
